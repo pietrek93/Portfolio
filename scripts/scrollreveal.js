7fdf387fd41f5f554aba3f2eb73efe2f82d6d34f
Piotr Nalepka <piotrekzwe@gmail.com>
2019-03-06 21:07:03 +0100

    portfolio

---
+++ scripts/scrollreveal.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal();
+		sr.reveal('.project-element, .icon_social, .skill-element',{
+			reset: true,
+			delay: 200,
+			distance: 0
+		});
+		sr.reveal('h1, h2, h3, p', {
+			reset: true,
+			delay: 200
+		});	
+	
